2c24e8cd60c6824601c8b0318519f833d5318a62
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-02-16 05:56:08 -0500

    cleanup breakpoint structure

--- breakpoint.py
+++ breakpoint.py
@@ -15,13 +15,10 @@ class Breakpoint:
         snap = {
             "TX": txID,
             "OUT": out,
-            "MASTER": {"BNB": 0, "LOK-3C0": 0, "RUNE-A1F": 0},
-            "USER-1": {"BNB": 0, "LOK-3C0": 0, "RUNE-A1F": 0},
-            "STAKER-1": {"BNB": 0, "LOK-3C0": 0, "RUNE-A1F": 0},
-            "STAKER-2": {"BNB": 0, "LOK-3C0": 0, "RUNE-A1F": 0},
+            "USER-1": {},
+            "STAKER-1": {},
+            "STAKER-2": {},
             "VAULT": {},
-            "POOL-BNB": None,
-            "POOL-LOK": None,
         }
 
         for name, acct in self.bnb.accounts.items():
@@ -33,15 +30,9 @@ class Breakpoint:
                 snap[name][coin.asset.symbol] = coin.amount
 
         for pool in self.thorchain.pools:
-            if pool.asset.is_equal("BNB.BNB"):
-                snap["POOL-BNB"] = {
-                    "BNB": int(pool.asset_balance),
-                    "RUNE-A1F": int(pool.rune_balance),
-                }
-            elif pool.asset.is_equal("BNB.LOK-3C0"):
-                snap["POOL-LOK"] = {
-                    "LOK-3C0": int(pool.asset_balance),
-                    "RUNE-A1F": int(pool.rune_balance),
-                }
+            snap['POOL.' + str(pool.asset)] = {
+                str(pool.asset): int(pool.asset_balance),
+                "RUNE-A1F": int(pool.rune_balance),
+            }
 
         return snap
