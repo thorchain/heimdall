9ba94706cd1a17b05771161bfe99dc1f6bc85b5a
Heimdall <heimdallthor02@gmail.com>
2020-11-14 08:45:24 +1100

    revert back to 30 attempts

--- scripts/smoke.py
+++ scripts/smoke.py
@@ -310,7 +310,7 @@ class Smoker:
         processed = False
         pending_txs = 0
 
-        for x in range(0, 60):  # 30 attempts
+        for x in range(0, 30):  # 30 attempts
             events = self.thorchain_client.events[:]
             sim_events = self.thorchain_state.events[:]
             new_events = events[len(sim_events) :]
