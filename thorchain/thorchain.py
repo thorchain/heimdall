6bce781201ae8a92ac0d0b4d94676627523c9e94
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-11-30 18:17:48 -0500

    fix linting

--- thorchain/thorchain.py
+++ thorchain/thorchain.py
@@ -512,11 +512,7 @@ class ThorchainState:
 
             out_txs.append(
                 Transaction(
-                    tx.chain,
-                    tx.to_address,
-                    tx.from_address,
-                    [coin],
-                    f"REFUND:{tx.id}",
+                    tx.chain, tx.to_address, tx.from_address, [coin], f"REFUND:{tx.id}",
                 )
             )
 
@@ -524,8 +520,7 @@ class ThorchainState:
 
         # generate event REFUND for the transaction
         event = Event(
-            "refund",
-            [{"code": code}, {"reason": reason}, *in_tx.get_attributes()],
+            "refund", [{"code": code}, {"reason": reason}, *in_tx.get_attributes()],
         )
 
         if tx.chain == "THOR":
@@ -1330,10 +1325,7 @@ class Pool(Jsonable):
         lp.pending_rune = 0
         lp.pending_asset = 0
         units = self._calc_liquidity_units(
-            self.rune_balance,
-            self.asset_balance,
-            rune_amt,
-            asset_amt,
+            self.rune_balance, self.asset_balance, rune_amt, asset_amt,
         )
 
         self.add(rune_amt, asset_amt)
